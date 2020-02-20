0a5011d25c0bd04db4e52b3acfb0c05e727d4688
christian salako <christian.s5547@gmail.com>
2020-02-20 20:57:20 +0000

    word game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,33 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
-  def about
+  def new
+    @letters = ('A'..'Z').to_a.sample(10)
+    @a = @letters[0]
+    @b = @letters[1]
+    @c = @letters[2]
+    @d = @letters[3]
+    @e = @letters[4]
+    @f = @letters[5]
+    @g = @letters[6]
+    @h = @letters[7]
+    @i = @letters[8]
+    @j = @letters[9]
+  end
+
+  def score
+  @url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
+  @user_serialized = open(@url).read
+  @user = JSON.parse(@user_serialized)
+
+  if @user["found"] == false
+    @user = "cant find it"
+  elsif @user["found"] == true
+    @user = "well done your word is valid"
+  end
+
   end
 end
+
+
